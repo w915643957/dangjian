58d0e2b923bd2f53c1bcfd07d112c9464785ba43
第二心跳 <915643957@qq.com>
2017-11-01 16:07:33 +0800

    新增党员发展、党员管理文件夹并加入单独页面

--- ht/js/module2.js
+++ ht/js/module2.js
@@ -1,24 +1,21 @@
 $(window).on("resize", function () {
     var height = $(window).height();
-    $(".left_nav").height(height - 147);
-    $(".child_page").height(height - 147);
+    $(".left_nav").height(height);
+    $(".child_page").height(height);
 })
 $(function () {
     var height = $(window).height();
-    $(".left_nav").height(height - 147);
-    $(".child_page").height(height - 147);
+    $(".left_nav").height(height);
+    $(".child_page").height(height);
 
 //        左侧菜单单击事件
     $('.left_nav').find('li>a').on('click', function () {
-        var i = $(this).find(".fa").eq(1);
-        i.toggleClass("fa fa-angle-up").toggleClass("fa fa-angle-down");
-        var ul = $(this).next('.nav-second-level');
-        if (ul.is(':hidden')) {
-            $('.nav-second-level').slideUp();
-            ul.slideDown();
-        } else {
-            ul.slideUp();
-        }
+        event.preventDefault();
+        var address = $(this).attr("href");
+        $('.left_nav').find('li>a').removeClass("act");
+        $(this).addClass("act");
+        $("iframe").attr("src", "");
+        $("iframe").attr("src", address);
     });
     $(".first_nav").find('li>a').on('click', function () {
         event.preventDefault();
